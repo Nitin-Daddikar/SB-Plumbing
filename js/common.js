ee20ace9073c96812f209758976a350baada19f6
Nitin Daddikar <clarus.nitin@gmail.com>
2025-11-21 00:29:45 +0530

    menu and other changes

--- js/common.js
+++ js/common.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
         .then(data => {
             document.getElementById('header').innerHTML = data;
             // Update active state in menu based on current page
-            // updateActiveMenuState();
+            updateActiveMenuState();
         });
 
     // Load footer
@@ -35,9 +35,69 @@ function updateActiveMenuState() {
     });
 
 
-    $("#cssmenu").menumaker({
+    // $("#cssmenu").menumaker({
+    //     title: "Menu",
+    //     format: "multitoggle"
+    // });
+
+    that = $("#cssmenu");
+    var cssmenu = $(that), settings = $.extend({
+      title: "Menu",
+      format: "multitoggle",
+      sticky: false
+    }, {
         title: "Menu",
         format: "multitoggle"
+    });
+
+    that.each(function () {
+      cssmenu.prepend('<div id="menu-button">' + settings.title + '</div>');
+      $(this).find("#menu-button").on('click', function () {
+        $(this).toggleClass('menu-opened');
+        var mainmenu = $(this).next('ul');
+        if (mainmenu.hasClass('open')) {
+          mainmenu.hide().removeClass('open');
+        }
+        else {
+          mainmenu.show().addClass('open');
+          if (settings.format === "dropdown") {
+            mainmenu.children('ul').show();
+          }
+        }
+      });
+
+      cssmenu.find('li ul').parent().addClass('has-sub');
+
+      multiTg = function () {
+        cssmenu.find(".has-sub").prepend('<span class="submenu-button"></span>');
+        cssmenu.find('.submenu-button').on('click', function () {
+          $(this).toggleClass('submenu-opened');
+          if ($(this).siblings('ul').hasClass('open')) {
+            $(this).siblings('ul').removeClass('open').hide();
+          }
+          else {
+            $(this).siblings('ul').addClass('open').show();
+          }
+        });
+      };
+
+      if (settings.format === 'multitoggle') multiTg();
+      else cssmenu.addClass('dropdown');
+
+      if (settings.sticky === true) cssmenu.css('position', 'fixed');
+
+      resizeFix = function () {
+        if ($(window).width() > 768) {
+          cssmenu.find('ul').show();
+        }
+
+        if ($(window).width() <= 768) {
+          cssmenu.find('ul').hide().removeClass('open');
+        }
+      };
+      resizeFix();
+      $(window).on('resize', resizeFix);
+
     });
 
     $("#cssmenu").prepend("<div id='menu-line'></div>");
